b06f2ecd03d0e9cebd64e47f2df81d7846e4eedc
bettagj <bettagj@spu.edu>
2020-11-24 14:37:33 -0800

    changed dotenv

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config() //Loads in process variables from .env
-// }
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config() //Loads in process variables from .env
+}
 
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express')
 const app = express()
 const flash = require('express-flash')
