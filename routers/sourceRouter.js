152c267c1e97465d182fa197f1c8a9e16b9fe6d7
MathewLister <listermathew0@gmail.com>
2020-11-24 20:19:51 -0800

    reset

--- routers/sourceRouter.js
+++ routers/sourceRouter.js
@@ -4,26 +4,27 @@ var path = require('path')
 const methodOverride = require('method-override')
 
 router.use(methodOverride('_method'))
-var dir = "/public/assets"
+router.use(express.static(__dirname + '/public/assets')) //Global variable so we can access stuff in /public
+// var dir = "/public/assets"
 
 router.get('/css/min', (req, res) => {
-    res.sendFile(dir + '/bootstrap-4.4.1/dist/css/bootstrap.min.css')
+    res.sendFile(__dirname + '/bootstrap-4.4.1/dist/css/bootstrap.min.css')
 })
 
 router.get('/css/dash', (req, res) => {
-    res.sendFile(dir + '/css/dashboard.css')
+    res.sendFile(__dirname + '/css/dashboard.css')
 })
 
 router.get('/js/popper', (req, res) => {
-    res.sendFile(dir + '/js/popper.min.js')
+    res.sendFile(__dirname + '/js/popper.min.js')
 })
 
 router.get('/js/min', (req, res) => {
-    res.sendFile(dir + '/bootstrap-4.4.1/dist/js/bootstrap.min.js')
+    res.sendFile(__dirname + '/bootstrap-4.4.1/dist/js/bootstrap.min.js')
 })
 
 router.get('/plant', (req, res) => {
-    res.sendFile('/home/bettagj/GMS/Website/public/images/Icons/plantIcon.svg')
+    res.sendFile(__dirname + '../images/Icons/plantIcon.svg')
 })
 
 module.exports = router
